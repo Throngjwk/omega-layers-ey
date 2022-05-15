abf7b1b5d6569cf3e53be01c26c04549832d4627
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-05-14 22:11:29 -0300

    Update meta-layer.js

--- docs/js/components/meta-layer.js
+++ docs/js/components/meta-layer.js
@@ -11,7 +11,7 @@ Vue.component("meta-layer", {
     computed: {
         showLayersPS: function()
         {
-            return this.metaLayer.getLayersPS().gte(1e24);
+            return this.metaLayer.getLayersPS().gte(1e10000000000000000000000000000);
         },
         showPowerers: function()
         {
